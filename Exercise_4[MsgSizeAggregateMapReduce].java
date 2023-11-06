3422ae15063e295cdd437073587c90d7862d3b14
Nandhakumar S J <111752462+Nandhakumarsj@users.noreply.github.com>
2023-11-06 11:12:58 +0530

    Examination Overview

---
+++ Exercise_4[MsgSizeAggregateMapReduce].java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+public class MsgSizeAggregateMapReduce
+ extends Configured implements Tool {
+public static void main(String[] args) throws Exception {
+int res = ToolRunner.run(new Configuration(),
+ new MsgSizeAggregateMapReduce(), args);
+System.exit(res);
+}
+@Override
+public int run(String[] args) throws Exception {
+if (args.length != 2) {
+System.err.println("Usage: <input_path>
+ <output_path>");
+System.exit(-1);
+}
